00d9a4cf5db35d2060a4db0dafcd64821ac4a614
Ilya <imoldavskiy@yansex.ru>
2022-11-11 14:35:02 +0300

    modulation.py add spectrum

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from digital import show_digital_signal,show_digital_signal_spectrum
 from modulation import *
 
 if __name__ == '__main__':
-    # create_amplitude_modulation(1, 1000, 10, 4)
+    # create_amplitude_modulation(2, 1000, 10, 2)
     # create_frequency_modulation(1, 1000, 4)
-    create_phase_modulation(1, 1000, 4)
+    # create_phase_modulation(1, 1000, 12)
+    # x, y = calc_frm_spectrum(1, 1000, 6)
+    show_spectrum()
+    # spectrum_synthesis(x, y)
      # frequencies_list = [1, 2, 4, 8]
 
     # show(1, 100, frequencies_list)
--- modulation.py
+++ modulation.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq, fftshift
+from scipy.fft import fft, fftfreq, fftshift, ifft
 from scipy import signal
 
 
@@ -28,14 +28,15 @@ def generate_digital_signal(duration, sampling_rate, chosen_frequency):
 
 
 def create_amplitude_modulation(duration, sampling_rate, chosen_frequency, A):
-    y_meander = generate_digital_signal(duration, sampling_rate, 10)
-    y_s = generate_wave(duration, sampling_rate, 80)
+    y_meander = generate_digital_signal(duration, sampling_rate, A)
+    y_s = generate_wave(duration, sampling_rate, A * 8)
     x = np.linspace(0, duration, sampling_rate * duration, endpoint=False)
 
     am_y = A * (y_meander * y_s)
 
     plt.plot(x, am_y)
     plt.show()
+    return am_y
 
 
 def create_frequency_modulation(duration, sampling_rate, chosen_frequency):
@@ -46,11 +47,12 @@ def create_frequency_modulation(duration, sampling_rate, chosen_frequency):
     fm_y = list()
     for i in range(len(x)):
         if y_meander[i] == 0:
-            fm_y.append(np.cos(2 * np.pi * x[i] * chosen_frequency))
+            fm_y.append(np.cos(2 * np.pi * x[i] * chosen_frequency * 6))
         else:
-            fm_y.append(np.cos(2 * np.pi * x[i] * (chosen_frequency * 6 + 10)))
+            fm_y.append(np.cos(2 * np.pi * x[i] * (chosen_frequency * 12)))
     plt.plot(x, fm_y)
     plt.show()
+    return fm_y
 
 
 def create_phase_modulation(duration, sampling_rate, chosen_frequency):
@@ -61,9 +63,75 @@ def create_phase_modulation(duration, sampling_rate, chosen_frequency):
     phm_y = list()
     for i in range(len(x)):
         if y_meander[i] == 0:
-            phm_y.append(np.cos(2 * np.pi * x[i] * chosen_frequency + 6))
+            phm_y.append(np.cos(2 * np.pi * x[i] * chosen_frequency + 12))
         else:
-            phm_y.append(np.cos(2 * np.pi * x[i] * chosen_frequency + 16))
+            phm_y.append(np.cos(2 * np.pi * x[i] * chosen_frequency + 22))
     plt.plot(x, phm_y)
     plt.show()
 
+
+def calc_am_spectrum(duration, sampling_rate, frequency):
+    x = np.linspace(0, duration, sampling_rate * duration, endpoint=False)
+    am_y = create_amplitude_modulation(duration, sampling_rate, frequency, 8)
+    yf = fft(am_y)
+    xf = fftfreq(sampling_rate, 1 / sampling_rate)[0: 220]
+    yf = yf[0: 220]
+    # plt.plot(xf, np.abs(yf))
+    # plt.show()
+    return xf, np.abs(yf)
+
+
+def calc_frm_spectrum(duration, sampling_rate, frequency):
+    x = np.linspace(0, duration, sampling_rate * duration, endpoint=False)
+    frm_y = create_frequency_modulation(duration, sampling_rate, frequency)
+    yf = fft(frm_y)
+    xf = fftfreq(sampling_rate, 1 / sampling_rate)[0: 220]
+    yf = yf[0: 220]
+    # plt.plot(xf, np.abs(yf))
+    # plt.show()
+    return xf, np.abs(yf)
+
+
+def calc_phm_spectrum(duration, sampling_rate, frequency):
+    x = np.linspace(0, duration, sampling_rate * duration, endpoint=False)
+    phm_y = create_frequency_modulation(duration, sampling_rate, frequency)
+    yf = fft(phm_y)
+    xf = fftfreq(sampling_rate, 1 / sampling_rate)[0: 220]
+    yf = yf[0: 220]
+    # plt.plot(xf, np.abs(yf))
+    # plt.show()
+    return xf, np.abs(yf)
+
+
+def show_spectrum():
+    # figure, axis = plt.subplots(3, 1)
+    am_x, am_y = calc_am_spectrum(1, 1000, 6)
+    fm_x, fm_y = calc_frm_spectrum(1, 1000, 6)
+    phm_x, phm_y = calc_phm_spectrum(1, 1000, 6)
+
+    plt.subplot(1, 3, 1)
+    plt.plot(am_x, am_y)
+    plt.title("Спектр амплитудной модуляции")
+
+    plt.subplot(1, 3, 2)
+    plt.plot(fm_x, fm_y)
+    plt.title("Спектр частотной модуляции")
+
+    plt.subplot(1, 3, 3)
+    plt.plot(phm_x, phm_y)
+    plt.title("Спектр фазовой модуляции")
+
+    plt.grid()
+    plt.show()
+
+
+
+
+
+def spectrum_synthesis(specter_x, specter_y):
+    # frs = sorted(specter_y, reverse=True)
+    # cleared_frs = [0 if i < len(frs) - 3 else i for i in frs]
+    cleared_frs = [0 if i < 216 else i for i in specter_y]
+    y = ifft(cleared_frs)
+    plt.plot(specter_x / len(cleared_frs), y)
+    plt.show()
